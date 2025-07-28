e437798c68a962974122d75e4a62fbaab2a77547
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-07-28 14:49:09 +0300

    Completed Chapter 4: Event Emitter

---
+++ 04TUT/index.js
@@ -0,0 +1,16 @@
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listner for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+    // emit event
+    myEmitter.emit("log", "Log event emitted!");
+}, 2000);
